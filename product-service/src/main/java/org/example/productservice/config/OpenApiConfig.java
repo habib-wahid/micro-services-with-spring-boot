c26fd3ef82d5dd67ef87c74922679d1cfaa66756
habib-wahid <habib11wahid@gmail.com>
2024-12-11 18:15:43 +0600

    swagger documentation is added

---
+++ product-service/src/main/java/org/example/productservice/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package org.example.productservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI orderOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Product-Service-Api")
+                        .description("This documentation contains all the apis")
+                        .version("1.0")
+                        .license(new License().name("Apache 2.0"))
+                );
+    }
+}
