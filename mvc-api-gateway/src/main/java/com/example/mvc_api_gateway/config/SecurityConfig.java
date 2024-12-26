e3fdcaa3223463f2ce407d4a73f114975d4d435c
habib-wahid <habib11wahid@gmail.com>
2024-12-26 12:54:19 +0600

    Keycloak auth server added and rest endpoints are secured

---
+++ mvc-api-gateway/src/main/java/com/example/mvc_api_gateway/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.mvc_api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       return http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2ResourceServer ->oauth2ResourceServer.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
